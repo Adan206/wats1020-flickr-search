9cfbad8fea878039c4cd1b5216e0e6b0504112ec
Adan206 <adanweli@hotmail.com>
2017-02-14 02:40:15 -0800

    new

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@
       $.each( data.items, function( i, item ) {
       
         var newListItem = $("<li>")
-        // If you're not doing the modal, then show info about the image.
+        
         var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
         var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
         var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
@@ -88,8 +88,8 @@
   });
 
   $('#infoModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var title = button.data('title'); // Extract info from data-* attributes
+    var button = $(event.relatedTarget); 
+    var title = button.data('title'); 
     var imgSrc = button.data('imgsrc');
     var imageDescription = button.data('description');
 
